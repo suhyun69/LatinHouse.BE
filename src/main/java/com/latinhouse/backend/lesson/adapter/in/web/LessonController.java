b3425a67c87555cbc95c400eab93336b937d78c2
suhyun.p69 <suhyun.p69@gmail.com>
2024-12-31 13:41:24 +0900

    fix: openapi document

--- src/main/java/com/latinhouse/backend/lesson/adapter/in/web/LessonController.java
+++ src/main/java/com/latinhouse/backend/lesson/adapter/in/web/LessonController.java
@@ -9,22 +9,28 @@ import com.latinhouse.backend.lesson.port.in.request.CreateLessonAppRequest;
 import com.latinhouse.backend.lesson.port.in.response.CreateLessonAppResponse;
 import com.latinhouse.backend.lesson.port.in.response.GetLessonAppResponse;
 import com.latinhouse.backend.lesson.port.in.response.LessonInfo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/lessons")
 @RequiredArgsConstructor
+@Tag(name = "Lesson", description = "Lesson API Document")
 public class LessonController {
 
     private final CreateLessonUseCase createLessonUseCase;
     private final GetLessonUseCase getLessonUseCase;
 
     @PostMapping
+    @Operation(summary = "수업 등록", description = "수업을 등록합니다")
     public ResponseEntity<CreateLessonWebResponse> createLesson(@RequestBody @Valid CreateLessonWebRequest webReq) {
         CreateLessonAppRequest appReq = new CreateLessonAppRequest(webReq);
         CreateLessonAppResponse appRes = createLessonUseCase.createLesson(appReq);
@@ -32,6 +38,7 @@ public class LessonController {
     }
 
     @GetMapping("/{no}")
+    @Operation(summary = "수업 조회", description = "수업을 조회합니다")
     public ResponseEntity<LessonInfo> getLessonByNo(@PathVariable Long no) {
         LessonInfo lessonInfo = getLessonUseCase.getLessonByNo(no);
         return ResponseEntity.ok(lessonInfo);
--- src/main/java/com/latinhouse/backend/profile/adapter/in/web/ProfileController.java
+++ src/main/java/com/latinhouse/backend/profile/adapter/in/web/ProfileController.java
@@ -9,16 +9,21 @@ import com.latinhouse.backend.profile.port.in.UpdateProfileUseCase;
 import com.latinhouse.backend.profile.port.in.request.CreateProfileAppRequest;
 import com.latinhouse.backend.profile.port.in.response.CreateProfileAppResponse;
 import com.latinhouse.backend.profile.port.in.response.GetProfileAppResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/profiles")
 @RequiredArgsConstructor
+@Tag(name = "Profile", description = "Profile API Document")
 public class ProfileController {
 
     private final CreateProfileUseCase createProfileUseCase;
@@ -26,6 +31,7 @@ public class ProfileController {
     private final UpdateProfileUseCase updateProfileUseCase;
 
     @PostMapping
+    @Operation(summary = "프로필 등록", description = "프로필을 등록합니다")
     public ResponseEntity<CreateProfileWebResponse> createProfile(@RequestBody @Valid CreateProfileWebRequest webReq) {
         CreateProfileAppRequest appReq = new CreateProfileAppRequest(webReq);
         CreateProfileAppResponse appRes = createProfileUseCase.createProfile(appReq);
@@ -34,6 +40,7 @@ public class ProfileController {
 
     // ID로 조회
     @GetMapping("/{id}")
+    @Operation(summary = "프로필 조회", description = "프로필을 조회합니다")
     public ResponseEntity<GetProfileWebResponse> getProfileById(@PathVariable String id) {
         GetProfileAppResponse appRes = getProfileUseCase.getProfileById(id);
         return ResponseEntity.ok(new GetProfileWebResponse(appRes));
@@ -41,6 +48,7 @@ public class ProfileController {
 
     // 모든 프로필 조회
     @GetMapping
+    @Operation(summary = "프로필 조회", description = "모든 프로필을 조회합니다")
     public ResponseEntity<List<GetProfileWebResponse>> getAllProfiles() {
         List<GetProfileAppResponse> appResList = getProfileUseCase.getAllProfiles();
         return ResponseEntity.ok(appResList.stream().map(GetProfileWebResponse::new).toList());
@@ -48,6 +56,7 @@ public class ProfileController {
 
     // 강사 등록
     @PutMapping("/{id}")
+    @Operation(summary = "강사 등록", description = "강사로 등록합니다")
     public ResponseEntity<GetProfileWebResponse> updateIsInstructor(@PathVariable String id) {
         GetProfileAppResponse appRes = updateProfileUseCase.updateIsInstructor(id);
         return ResponseEntity.ok(new GetProfileWebResponse(appRes));
